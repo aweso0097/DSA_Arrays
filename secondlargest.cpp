49bc37145d21992ae1fa183d9ecd17928e3bb9bf
cse23035@iiitkalyani.ac.in <cse23035@iiitkalyani.ac.in>
2026-05-25 10:52:59 +0530

    Handle edge case for single element vectors

--- secondlargest.cpp
+++ secondlargest.cpp
@@ -2,13 +2,16 @@
 using namespace std;
 
 pair<int,int> secondelement(vector <int> &arr) {
+    if (arr.size() == 1) {
+        return {-1,-1};
+    }
     sort(arr.begin(),arr.end());
     int second_min = arr[1];
     int second_max = arr[arr.size()-2];
     return {second_min,second_max};
 }
 int main () {
-    vector<int> arr1 = {23,45,67,21,44,69,54,66,92,78,82};
+    vector<int> arr1 = {82,45,67,89,21,56,78,43,55,67,90,31,94,85};
     pair<int,int> result = secondelement(arr1);
     cout <<"The second smallest element is  " << result.first << endl;
     cout <<"The second largest element is  " << result.second;
