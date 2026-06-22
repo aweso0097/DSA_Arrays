4e42feda865dc48a77809e3d0d435477c37d919e
aweso0097 <aweso1045@gmail.com>
2026-06-22 19:59:32 +0530

    Implement Moore's Voting Algorithm for majority element
    This is optimal solution for the majority element in an array(>N/2)....Space complexity is O(N).

---
+++ moore's_voting.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int count = 0;
+        int element;
+        int n= nums.size();
+        for (int i =0;i<n;i++) {
+            if (count==0) {
+                element = nums[i];
+                count = 1;
+            }
+            else if (nums[i] == element){
+                count++;
+            }
+            else {
+                count--;
+            }
+
+        }
+        int count1 = 0;
+        for(int i =0;i<n;i++) {
+            if (nums[i] == element) {
+                count1++;
+            }
+            if(count1 > n/2) {
+                return element;
+            }
+        }
+        return -1;
+    }
+};
