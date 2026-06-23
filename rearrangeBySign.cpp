6b1c66f420cd734a73af215e95beb547a6c739e5
aweso0097 <aweso1045@gmail.com>
2026-06-23 23:18:06 +0530

    Implement rearrangeArray to alternate signs

---
+++ rearrangeBySign.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int posindex = 0;
+        int negindex = 1;
+        vector<int> result(nums.size());
+        for(int i =0;i<nums.size();i++) {
+            if (nums[i] > 0) {
+                result[posindex] = nums[i];
+                posindex += 2;
+            }
+            else {
+                result[negindex] = nums[i];
+                negindex += 2; 
+            }
+
+        }
+        return result;
+    }
+};
